62275de924a0cf1f20bcdd604f373d5c927f63a9
CristianG1h <94920291+CristianG1h@users.noreply.github.com>
2026-08-20 11:51:51 -0500

    feat: agregar índice de recursos visuales de custodia

---
+++ src/data/custodia/assets.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = {
+  membrete: [
+    require("./assets.parts/membrete-01"),
+    require("./assets.parts/membrete-02"),
+    require("./assets.parts/membrete-03"),
+    require("./assets.parts/membrete-04"),
+    require("./assets.parts/membrete-05"),
+    require("./assets.parts/membrete-06"),
+    require("./assets.parts/membrete-07"),
+    require("./assets.parts/membrete-08"),
+  ].join(""),
+  firma: [
+    require("./assets.parts/firma-01"),
+    require("./assets.parts/firma-02"),
+  ].join(""),
+};
